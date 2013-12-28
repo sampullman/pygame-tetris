2422c5a7da564145539df135ca8d0c6794db26f8
Sam Pullman <sampullman@Sams-MacBook-Pro.local>
2013-12-28 02:59:57 -0800

    functioning screen initialization code with infinite loop

--- tetris.py
+++ tetris.py
@@ -1,13 +1,20 @@
 import random, os.path, sys
 import pygame
+from pygame.locals import *
 
 main_dir = os.path.split(os.path.abspath(__file__))[0]  # Program's diretory
+
+FPS = 25
+SCREENRECT = Rect(0, 0, 640, 600)
 
 def main():
     # Initialize SDL components
     pygame.init()
     screen = pygame.display.set_mode(SCREENRECT.size, 0)
     clock = pygame.time.Clock()
+
+    while True:
+        clock.tick(FPS)
 
 #if python says run, let's run!
 if __name__ == '__main__':
